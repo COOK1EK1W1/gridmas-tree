9bc5c671de026efe917431530f5682e7f859d43b
Ciaran Cook <ciarancook1@gmail.com>
2023-11-19 00:57:53 +0000

    added twinkle

--- backend/strip_anim.py
+++ backend/strip_anim.py
@@ -1,4 +1,5 @@
 import util
+import random
 import time
 import threading
 
@@ -12,3 +13,18 @@ def doStandard(stopFlag: threading.Event):
                 util.setLight(ia, (int(r/1.1), int(g/1.1), int(b/1.1)))
             util.update()
             time.sleep(0.01)
+            if (stopFlag.is_set()):
+                break
+
+def doTwinkle(stopFlag: threading.Event):
+    while not stopFlag.is_set():
+        x = random.randint(0, 499)
+        util.setLight(x, (0, 100, 0))
+        for ia in range(util.pixel_num):
+            r, g, b = util.get_light(ia)
+            color = (min(int(r+5), 100), min(int(g+5),255), min(20, int(b+5)))
+            util.setLight(ia, color)
+        util.update()
+        time.sleep(0.02)
+        if (stopFlag.is_set()):
+            break
