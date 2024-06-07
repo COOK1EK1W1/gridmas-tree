2dc5ef8c32087fa7e88b691672bba8a4cd0b1d4f
COOK1EK1W1 <61564769+COOK1EK1W1@users.noreply.github.com>
2024-06-07 15:11:26 +0100

    fixed webserver and color order

--- backend/main.py
+++ backend/main.py
@@ -4,6 +4,17 @@ from tree import tree
 from webServer import app
 import threading
 
+from dotenv import load_dotenv
+import os
+
+    
+
 if __name__ == '__main__':
-    threading.Thread(target=app.run, kwargs={'debug': True, 'host': "0.0.0.0", "use_reloader": False, "port": 3000}).start()
+    load_dotenv()
+    port = os.environ.get("PORT")
+    if port is None:
+        print("no PORT environment variable, trying 3000")
+        port = 3000
+    else:
+        threading.Thread(target=app.run, kwargs={'debug': True, 'host': "0.0.0.0", "use_reloader": False, "port": port}).start()
     tree.run()
