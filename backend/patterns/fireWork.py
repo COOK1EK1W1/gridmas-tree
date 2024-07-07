ce8402a5c77484d52103d52d5cc6c2b4a72e068a
COOK1EK1W1 <61564769+COOK1EK1W1@users.noreply.github.com>
2024-07-07 15:23:46 +0100

    added fireworks

---
+++ backend/patterns/fireWork.py
@@ -0,0 +1,51 @@
+import random
+from colors import Color
+from util import pythagorasDistance
+from tree import tree
+
+name = "Fire Works"
+author = "Ciaran"
+
+
+class Explosion:
+    def __init__(self, x, y, z, max_age):
+        self.x = x
+        self.y = y
+        self.z = z
+        self.max_age = max_age
+        self.tick = 0
+        self.color = Color.random()
+
+
+def run():
+    explosions: list[Explosion] = []
+    i = 0
+    interval = 50
+    while True:
+        if i == 0:
+            random_x_coord = (random.randrange(-100, 100, 1)) / 100
+            random_y_coord = (random.randrange(-100, 100, 1)) / 100
+            random_z_coord = (random.randrange(0, int(tree.height * 100), 1)) / 100
+            explosions.append(Explosion(random_x_coord, random_y_coord, random_z_coord, 40))
+            interval = random.randrange(40, 100)
+        i = (i + 1) % interval
+
+        print(explosions)
+        for exp in explosions:
+            exp.tick += 1
+
+        for pixel in tree.pixels:
+            a = random.random()
+            if a > 0.8:
+                pixel.fade(random.randrange(100, 120, 1) / 100)
+            elif a > 0.77:
+                pixel.set_color(Color.black())
+            elif a > 0.76:
+                pixel.fade(0.7)
+
+            for exp in explosions:
+                if exp.tick < pythagorasDistance([exp.x, exp.y, exp.z], [pixel.x, pixel.y, pixel.z]) * 100 < exp.tick * 1.8:
+                    pixel.set_color(exp.color)
+
+        tree.update()
+        explosions = list(filter(lambda x: x.tick <= x.max_age, explosions))
--- scanning/main.py
+++ scanning/main.py
@@ -1,39 +1,67 @@
-from cv2 import VideoCapture, imwrite
+from cv2 import VideoCapture, imwrite, imshow, waitKey, destroyAllWindows
+import sys
 
 import requests
 import time
 
 print("init camera")
 
-cam_port = 2
+cam_port = 0
 cam = VideoCapture(cam_port)
 
+url = "http://192.168.1.50"
+
 print("clearing light")
-x = requests.get("http://192.168.1.50:5000/lightoff")
+
+x = requests.get(url + "/lightoff")
 if x.status_code != 200:
     raise Exception("No Light Server")
 
+for d in range(4):
+    for a in range(10):
+        for x in range(500):
+            if x & (1 << a):
+                x = requests.get(f"{url}/lighton/{x}")
+            else:
+                x = requests.get(f"{url}/lightoff/{x}")
+
+        time.sleep(0.5)
+        result, image = cam.read()
+        time.sleep(0.5)
+        imwrite(f"scanning/results/results2-{a}-{d*90}.png", image)
+
+    input("rotate")
+
+"""
+
 time.sleep(0.5)
-for i in range(100):
-    print(f"locating light {i}", end="\r")
-    x = requests.get(f"http://192.168.1.50:5000/lighton/{i}")
-    if x.status_code != 200:
-        raise Exception("No Light Server")
+for d in range(4):
+    for i in range(500):
+        print(f"locating light {i}", end="\r")
+        x = requests.get(f"{url}/lighton/{i}")
+        if x.status_code != 200:
+            raise Exception("No Light Server")
 
-    time.sleep(0.5)
-    result, image = cam.read()
-    time.sleep(0.5)
+        time.sleep(0.5)
+        result, image = cam.read()
+        time.sleep(0.5)
 
-    if result:
+        if result:
 
-        # imshow("test", image)
+            imshow("test", image)
+            waitKey(0)
+            destroyAllWindows()
+            
 
-        imwrite(f"scanning/results/results-{i}-0.png", image)
+            imwrite(f"scanning/results/results-{i}-{d*90}.png", image)
 
-    else:
-        raise Exception("camera error")
+        else:
+            raise Exception("camera error")
 
-    x = requests.get(f"http://192.168.1.50:5000/lightoff/{i}")
-    if x.status_code != 200:
-        raise Exception("No Light Server")
-    print(f"Light {i} located  ")
+        x = requests.get(f"{url}/lightoff/{i}")
+        if x.status_code != 200:
+            raise Exception("No Light Server")
+        print(f"Light {i} located  ")
+    input("please turn tree")
+
+"""
