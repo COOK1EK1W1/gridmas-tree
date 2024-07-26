749470a79ee66822e4e00868ab185925031327b0
COOK1EK1W1 <61564769+COOK1EK1W1@users.noreply.github.com>
2024-07-26 18:53:17 +0100

    improved scanning

--- scanning/main.py
+++ scanning/main.py
@@ -1,67 +1,217 @@
 from cv2 import VideoCapture, imwrite, imshow, waitKey, destroyAllWindows
-import sys
 
+import cv2
+from cv2.typing import MatLike, Point
 import requests
 import time
 
 print("init camera")
 
 cam_port = 0
+
+url = "http://192.168.1.249"
+
+light_amount = 350
+
+
+
+
 cam = VideoCapture(cam_port)
 
-url = "http://192.168.1.50"
 
-print("clearing light")
+def get_photo_of(lights: list[list[int]]) -> MatLike:
+    requests.post(f"{url}/setalllight", data=str(lights))
 
-x = requests.get(url + "/lightoff")
-if x.status_code != 200:
-    raise Exception("No Light Server")
+    time.sleep(0.1)
+    result, image = cam.read()
+    time.sleep(0.1)
+    return image
 
-for d in range(4):
-    for a in range(10):
-        for x in range(500):
-            if x & (1 << a):
-                x = requests.get(f"{url}/lighton/{x}")
+
+def clear_tree():
+    print("clearing light")
+    x = requests.get(url + "/lightoff")
+    if x.status_code != 200:
+        raise Exception("No Light Server")
+
+
+def countdown(seconds: int):
+    for i in range(seconds):
+        print(seconds - i, end="\r")
+        time.sleep(1)
+
+
+dirs = [0, 90, 180, 270]
+
+
+def find_light_loc(image: MatLike):
+    gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
+    minVal, maxVal, minLoc, maxLoc = cv2.minMaxLoc(gray)
+    if maxVal > 100:
+        return maxLoc
+    else:
+        return None
+
+
+def find_lights(all_on: MatLike, all_off: MatLike, images: list[MatLike]) -> list[Point | None]:
+    all_on_subtracted = cv2.subtract(all_on, all_off)
+
+    subtracted_images = list(map(lambda x: cv2.subtract(all_on, x), images))
+
+    locations: list[Point | None] = []
+
+    for pixelid in range(light_amount):
+        current_image = all_on_subtracted
+        for i, image in enumerate(subtracted_images):
+            if (pixelid & 0x1 << i) == 0:
+                current_image = cv2.subtract(current_image, image)
             else:
-                x = requests.get(f"{url}/lightoff/{x}")
+                current_image = cv2.subtract(current_image, cv2.subtract(all_on_subtracted, image))
 
-        time.sleep(0.5)
+        locations.append(find_light_loc(current_image))
+    return locations
+
+
+def find_individual(pixels: list[int], revalidate_clean_plate: int = 5) -> list[Point | None]:
+    positions: list[Point | None] = []
+    clear_tree()
+    colors = [[0, 0, 0] for _ in range(light_amount)]
+    clean_plate = get_photo_of(colors)
+    i = 0
+    for pixel in pixels:
+        if i % revalidate_clean_plate == 0:
+            colors = [[0, 0, 0] for _ in range(light_amount)]
+            clean_plate = get_photo_of(colors)
+        print(f"finding {i}", end="\r")
+        colors = [[0, 0, 0] for _ in range(light_amount)]
+        colors[pixel] = [255, 255, 255]
+        new_photo = get_photo_of(colors)
+        isolated = cv2.subtract(new_photo, clean_plate)
+        positions.append(find_light_loc(isolated))
+        i += 1
+    return positions
+
+
+def scan(dir: int):
+    countdown(3)
+    dir_degrees = dirs[int(dir) - 1]
+
+    # get all lights on
+    all_on = get_photo_of([[255, 255, 255] for _ in range(light_amount)])
+    print("on")
+    imwrite(f"results/results2-on-{dir_degrees}.png", all_on)
+
+    # get all lights off
+    all_off = get_photo_of([[0, 0, 0] for _ in range(light_amount)])
+    print("off")
+    imwrite(f"results/results2-off-{dir_degrees}.png", all_off)
+
+    images: list[MatLike] = []
+
+    for a in range(light_amount.bit_length()):
+        colors: list[list[int]] = []
+        for x in range(light_amount):
+            if x & (1 << a) != 0:
+                colors.append([255, 255, 255])
+            else:
+                colors.append([0, 0, 0])
+
+        image = get_photo_of(colors)
+        images.append(image)
+        print(1 << a)
+        imwrite(f"results/results2-{a}-{dir_degrees}.png", image)
+
+    locations = find_lights(all_on, all_off, images)
+
+    count = len(locations) - locations.count(None)
+    print(f"locations found for {count} / {len(locations)}")
+
+    none_positions = [index for index, location in enumerate(locations) if location is None]
+
+    fixed_positions = find_individual(none_positions)
+    for i, position in enumerate(none_positions):
+        locations[position] = fixed_positions[i]
+
+    count = len(locations) - locations.count(None)
+    print(f"locations found for {count} / {len(locations)}")
+
+    for location in locations:
+        if location is not None:
+            cv2.circle(all_on, location, 6, (255, 0, 0), 3)
+    print(locations)
+    cv2.imshow("test", all_on)
+    waitKey(20)
+
+
+def camera_test():
+    while True:
         result, image = cam.read()
-        time.sleep(0.5)
-        imwrite(f"scanning/results/results2-{a}-{d*90}.png", image)
-
-    input("rotate")
-
-"""
-
-time.sleep(0.5)
-for d in range(4):
-    for i in range(500):
-        print(f"locating light {i}", end="\r")
-        x = requests.get(f"{url}/lighton/{i}")
-        if x.status_code != 200:
-            raise Exception("No Light Server")
-
-        time.sleep(0.5)
-        result, image = cam.read()
-        time.sleep(0.5)
-
-        if result:
-
-            imshow("test", image)
-            waitKey(0)
-            destroyAllWindows()
+        cv2.line(image, (image.shape[1] // 2, 0), (image.shape[1] // 2, image.shape[0]), (100, 0, 0), 2)
+        imshow("test", image)
+        key = waitKey(20)
+        if key == 27:
+            break
 
 
-            imwrite(f"scanning/results/results-{i}-{d*90}.png", image)
+def run(option: int):
+    if option == 5:
+        camera_test()
+    elif 0 < option < 5:
+        scan(option)
+    elif option == 6:
 
-        else:
-            raise Exception("camera error")
+        imagedirs = [
+            "results/results2-0-0.png",
+            "results/results2-1-0.png",
+            "results/results2-2-0.png",
+            "results/results2-3-0.png",
+            "results/results2-4-0.png",
+            "results/results2-5-0.png",
+            "results/results2-6-0.png",
+            "results/results2-7-0.png",
+            "results/results2-8-0.png",
+        ]
 
-        x = requests.get(f"{url}/lightoff/{i}")
-        if x.status_code != 200:
-            raise Exception("No Light Server")
-        print(f"Light {i} located  ")
-    input("please turn tree")
+        cleanplate = "results/results2-off-0.png"
+        cleanimage = cv2.imread(cleanplate)
 
-"""
+        all_on = cv2.imread("results/results2-on-0.png")
+        images = list(map(lambda x: cv2.imread(x), imagedirs))
+        locations = find_lights(all_on, cleanimage, images)
+        for location in locations:
+            if location is not None:
+                cv2.circle(all_on, location, 6, (255, 0, 0), 3)
+        cv2.imshow("test", all_on)
+        print(locations)
+        count = len(locations) - locations.count(None)
+        print(f"locations found for {count} / {len(locations)}")
+        waitKey(20)
+    elif option == 7:
+        countdown(2)
+        all_on = get_photo_of([[255, 255, 255] for _ in range(light_amount)])
+        all_off = get_photo_of([[0, 0, 0] for _ in range(light_amount)])
+        imshow("test", cv2.subtract(all_on, all_off))
+        waitKey(20)
+
+
+
+if __name__ == "__main__":
+    while True:
+        clear_tree()
+        print("""please select a function:
+    1: scan x+
+    2: scan y+
+    3: scan x-
+    4: scan y-
+    5: camera test
+    6: pixel find test
+    7: difference test""")
+        option = input("choose:")
+        try:
+            if not int(option):
+                continue
+
+            run(int(option))
+
+        except Exception as err:
+            print(err)
