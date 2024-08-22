41ff29bbec07749df2512d0ba1a95ad383e4b295
COOK1EK1W1 <61564769+COOK1EK1W1@users.noreply.github.com>
2024-08-22 21:41:49 +0100

    fixed bad apple

--- backend/patterns/rick.py
+++ backend/patterns/rick.py
@@ -8,10 +8,13 @@ author = "Ciaran"
 def run():
     cap = cv2.VideoCapture("patterns/badapple.mp4")
     coords = []
+
     for pixel in tree.pixels:
         videox = max(min(int(((-pixel.x + 1) / 2) * 480), 479), 0)
         videoy = max(min(int((1 - pixel.z / 2) * 360), 359), 0)
         coords.append((videox, videoy))
+
+    tree.set_fps(30)
 
     while True:
 
@@ -21,7 +24,11 @@ def run():
 
         for i, pixel in enumerate(tree.pixels):
             if 0.5 < pixel.z < 2.5:
-                pixel.set_rgb(*frame[coords[i][1]][coords[i][0]])
+                if frame[coords[i][1]][coords[i][0]].any():
+                    pixel.set_rgb(150, 150, 150)
+                else:
+                    pixel.set_rgb(0, 0, 0)
+
             else:
                 pixel.set_rgb(0, 0, 0)
         tree.update()
